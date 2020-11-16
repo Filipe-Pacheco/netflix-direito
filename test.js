1f3085c244bf89ea13f8ca1fe03e0c0124bb4cf0
Filipe Pacheco <52587073+Filipe-Pacheco@users.noreply.github.com>
2020-11-16 12:59:17 -0300

    9

---
+++ test.js
@@ -0,0 +1,14 @@
+
+
+function teste() {
+    console.log('FUNCIONA');
+console.log('FUNCIONA');
+console.log('FUNCIONA');
+console.log('FUNCIONA');
+};
+
+export default teste();
+
+var variavel = "Hello world";
+
+export default variavel;
